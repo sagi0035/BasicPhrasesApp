d5d07ca92d962bd821dbedf5c756e10080a8bafa
seana <seanagius2@gmail.com>
2020-11-29 16:45:03 +0100

    Translator

---
+++ app/src/main/java/com/example/seana/basicphrases/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.seana.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        // so first we will get what we are clicking (which will be a button)
+        Button button = (Button) view;
+        Log.i("Button Pressed",button.getTag().toString());
+
+        // now we get the file that we wish to place in the circumstance
+        // now this is going to be based on a tag which we will want to put inside of a v#ariable
+        MediaPlayer mPlayer = MediaPlayer.create(this, getResources().getIdentifier(button.getTag().toString(), "raw", getPackageName()));
+        mPlayer.start();
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
